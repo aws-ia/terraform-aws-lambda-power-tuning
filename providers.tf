99e6c6127d1fb4380c750153af6720d8acf0b57c
sfloresk <sfkanter@amazon.com>
2024-07-03 16:06:51 -0600

    add version constraint for provider

--- providers.tf
+++ providers.tf
@@ -6,5 +6,13 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.26"
     }
+    archive = {
+      source = "hashicorp/archive"
+      version = ">= 2.4.2"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = ">= 3.2.2"
+    }
   }
 }
