969676d05a38a39bed4b00e77ef8b902a6838e68
André Stoll <andre.stoll@hotmail.com>
2025-06-05 12:00:37 +0200

    Update versions for tf providers and tflint

--- providers.tf
+++ providers.tf
@@ -4,15 +4,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.26"
+      version = ">= 5.99"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = ">= 2.4.2"
+      version = ">= 2.7.1"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.2"
+      version = ">= 3.2.4"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5.3"
     }
   }
 }
